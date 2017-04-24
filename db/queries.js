9c8625c490d8f25c7f59b0ea1cfb4f84060ddafe
Jordan Doig <jordandoig@live.com>
2017-04-24 15:36:57 -0600

    Added changePassword function to queries

--- db/queries.js
+++ db/queries.js
@@ -10,10 +10,15 @@ function addVote(data) {
 
 function deletePixelArt(data) {
   return pg('pixel_art').where('id', data.id).del();
-}
+};
+
+function changePassword(data) {
+  return pg('user').where('id', data.id).update('password', data.password);
+};
 
 module.exports = {
   addPixelArt,
   addVote,
   deletePixelArt,
+  changePassword,
 }
