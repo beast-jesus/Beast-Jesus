708256d21d49974d66636653f6ffb0717f0b4330
Jordan Doig <jordandoig@live.com>
2017-04-24 11:48:01 -0600

    Changed localhost db to lowercase

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/Beast-Jesus'
+    connection: 'postgres://localhost/beast-jesus'
   },
 
   production: {
