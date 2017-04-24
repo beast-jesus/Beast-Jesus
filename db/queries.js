9c0ba322cd9e027e764f1f5fd873003f34b7754a
Jordan Doig <jordandoig@live.com>
2017-04-24 15:32:23 -0600

    Added deletePixelArt function to queries

--- db/queries.js
+++ db/queries.js
@@ -8,6 +8,12 @@ function addVote(data) {
   return pg('vote').insert(data);
 };
 
+function deletePixelArt(data) {
+  return pg('pixel_art').where('id', data.id).del();
+}
+
 module.exports = {
   addPixelArt,
+  addVote,
+  deletePixelArt,
 }
