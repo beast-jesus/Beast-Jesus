337e313f1e898fc285a72e7832ca95ad4aba3d42
Jordan Doig <jordandoig@live.com>
2017-04-25 12:57:13 -0600

    Added last_name to user, changed name to first_name

--- migrations/20170423194130_user.js
+++ migrations/20170423194130_user.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('user', function(table){
       table.increments();
-      table.string('name').notNullable();
+      table.string('first_name').notNullable();
+      table.string('last_name').notNullable();
       table.string('email').notNullable().unique();
       table.string('username').notNullable().unique();
       table.string('password').notNullable();
