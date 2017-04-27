fccdea7f76de8655ddace4eece945e7d39c871c7
MOLLY BROOKS <MOLLYJBROOKS@GMAIL.COM>
2017-04-27 15:00:23 -0600

    changed routes and handlebars

--- routes/pixel_page.js
+++ routes/pixel_page.js
@@ -6,9 +6,13 @@ router.get('/', (req, res) => {
   console.log("hit route")
   queries.getPainting()
   .then(data => {
+
+    var randomIndex = [Math.floor(Math.random() * ((data.length)-1))]
+    data=data[randomIndex]
     console.log(data)
     res.render('pixel_page', {data})
   })
 })
+
 
 module.exports = router;
