5f375ba1cb1b7d518e3e6c1f93c4a02364b08a83
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-07-05 14:33:29 -0600

    linked artwork and pixel art together

--- routes/gallery.js
+++ routes/gallery.js
@@ -2,31 +2,28 @@ var express = require('express');
 var router = express.Router();
 var queries = require('../db/queries')
 
+// router.get('/', (req, res) => {
+//   console.log("gotcha")
+//   queries.getPainting()
+//   .then((data) => {
+//     res.render('gallery', {data})
+//   })
+// })
 router.get('/', (req, res) => {
-  console.log("hit route")
-  queries.getPainting()
-  .then(data => {
-    res.render('gallery', {data})
+console.log('gotcha')
+  queries.getPixels()
+  .then(item =>{
+    res.render('gallery', {item})
   })
 })
 
+router.get('/:id/delete', (req, res, next) => {
+  console.log("hit route")
+  queries.deletePixelArt(req.params.id)
+  .then(() => {
+    res.redirect('/gallery')
+  })
+})
 
-router.post('/addPixelArt', (req, res) => {
-    var data = {};
-    document.getElementByClassName('cell').each(function () {
-        var pixClass = this.class;
-        console.log(pixClass)
-        if (pixClass.length > 4) {
-            data[this.id] = pixClass.slice(5);
-        }
-    });
-    req.body.div_data = data;
-    queries.addPixelArt(req.body)
-        .then(data => {
-            res.render('gallery', {
-                data
-            });
-        });
-});
 
 module.exports = router;
